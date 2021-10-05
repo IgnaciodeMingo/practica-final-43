84045466557d4961f341a30f4030e9533cae8b95
jonathan-alvarez <dragonxxxxzzzz@gmail.com>
2021-10-04 22:57:21 -0300

    principio

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,32 @@
-const App = () => <h1>App</h1>;
+import {useState} from 'react';
+
+
+const App = () => {
+  const [numero, setNumero] = useState(0);
+  const dec = () => {
+    setNumero(prev => prev-1);
+  }
+  const inc = () => {
+    setNumero(prev => prev+1);
+  }
+  const retd = () => {
+    setNumero(prev => 0);
+  }
+    return(
+    <div>
+
+      <h1>{numero}</h1>
+
+      <button onClick={inc}>+</button>
+
+      <button onClick={dec}>-</button>
+
+      <button onClick={retd}>--</button>
+
+    </div>
+
+    );
+
+};
 
 export default App;
